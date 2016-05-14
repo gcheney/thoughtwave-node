171ba9892114bc1c9c14fcf5eea535cdf65a9d7c
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-13 22:21:23 -0500

    Update gulpfile with inject and nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require('gulp');
 var jshint  = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var nodemon = require('gulp-nodemon');
 
 
 var jsFiles = ['*.js', 'src/**/*.js', 'public/js/*.js'];
@@ -9,4 +10,41 @@ gulp.task('style', function() {
    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('inject', function () {
+    var widedep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {
+        read: false
+    });
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/index.ejs')
+        .pipe(widedep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: './bin/www',
+        delayTime: 1,
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (en) {
+            console.log('Restarting...');
+        });
 });
