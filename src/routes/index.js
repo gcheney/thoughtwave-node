ca420019a14400f746502d0b3174856f3d53d889
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-13 22:56:28 -0500

    update partial views

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('../views/index', { title: 'Express' });
+  res.render('../views/home/index', { title: 'Coder Tutorials' });
 });
 
 module.exports = router;
