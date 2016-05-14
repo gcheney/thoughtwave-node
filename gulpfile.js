98081843ece634ab14de5d3c5412c3638054b4e4
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-14 13:14:15 -0500

    Setup minification and concatination for css and js in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,31 @@
-var gulp      = require('gulp'),
-    jshint    = require('gulp-jshint'),
-    stylish   = require('jshint-stylish'),
-    nodemon   = require('gulp-nodemon'),
-    useref    = require('gulp-useref'),
-    gulpif    = require('gulp-if'),
-    uglify    = require('gulp-uglify'),
-    cssnano   = require('gulp-cssnano'),
-    del       = require('del');
+var gulp        = require('gulp'),
+    jshint      = require('gulp-jshint'),
+    stylish     = require('jshint-stylish'),
+    nodemon     = require('gulp-nodemon'),
+    inject      = require('gulp-inject'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    cssnano     = require('gulp-cssnano'),
+    runSequence = require('run-sequence');
 
-var jsFiles = ['*.js', 'src/**/*.js', 'public/js/*.js'],
-    myFiles = ['./public/css/*.css', './public/js/*.js'];
+var jsFiles = ['*.js', 'src/**/*.js', 'public/js/*.js'];
+
+var source = './app_server/views/partials/*.ejs';
+var dest = './app_server/views/partials';
 
 gulp.task('style', function() {
-   return gulp.src(jsFiles)
+    console.log('Checking coding style...');
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('inject', function () {
-    var widedep = require('wiredep').stream;
-    var inject = require('gulp-inject');
+    console.log('Injecting static files...');
+    var minFiles = ['./public/css/*.min.css', './public/js/*.min.js'];
 
-    var injectSrc = gulp.src(myFiles, {
-        read: false
-    });
+    var injectSrc = gulp.src(minFiles, { read: false });
+    
     var injectOptions = {
         ignorePath: '/public'
     };
@@ -33,34 +35,48 @@ gulp.task('inject', function () {
         directory: './public/lib',
         ignorePath: '../../public'
     };
+    
+    var wiredep = require('wiredep').stream;
 
-    return gulp.src('./app_server/views/partials/*.ejs')
-        .pipe(widedep(options))
+    return gulp.src(source)
+        .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
-        .pipe(gulp.dest('./app_server/views/partials'));
+        .pipe(gulp.dest(dest));
 });
 
-gulp.task('useref', function () {
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', cssnano()))
-        .pipe(gulp.dest('public/dist'));
+gulp.task('css', function() {
+    console.log('Compressing CSS...');
+    return gulp.src('public/css/*.css')
+        .pipe(cssnano())
+        .pipe(concat('styles.min.css'))
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
-})
+gulp.task('js', function () {
+    console.log('Compressing JS...');
+    return gulp.src('./public/js/*.js')
+        .pipe(uglify())
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest('public/js'));
+});
 
-gulp.task('serve', ['style', 'inject', 'useref'], function () {
-        var options = {
-            script: './bin/www',
-            delayTime: 1,
-            watch: jsFiles
-        };
+gulp.task('build', function (callback) {
+    console.log('Building files...');
+    runSequence(['style', 'css', 'js'],
+                'inject',
+                callback);
+});
 
-        return nodemon(options)
-            .on('restart', function (en) {
-                console.log('Restarting...');
-            });
+gulp.task('serve', ['build'], function () {
+    console.log('Serving a gulp...');
+    var options = {
+        script: './bin/www',
+        delayTime: 1,
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (en) {
+            console.log('Restarting...');
+        });
 });
