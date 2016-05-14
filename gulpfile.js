875d458e4d3a250c34591aec18de5381f39773fa
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-13 20:38:50 -0500

    Create gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
+var gulp    = require('gulp');
+var jshint  = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
-var jsFiles = ['*.js', 'src/**/*.js'];
+
+var jsFiles = ['*.js', 'src/**/*.js', 'public/js/*.js'];
 
 gulp.task('style', function() {
    return gulp.src(jsFiles)
         .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {
-            verbose: true
-        }));
+        .pipe(jshint.reporter(stylish));
 });
