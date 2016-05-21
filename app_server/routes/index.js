75398def15613f0bdf306c6fa0b761f1e1390a10
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-21 02:16:10 -0500

    Setup read view template wells and comments

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -12,6 +12,6 @@ router.get('/contact', ctrlHome.contact);
 
 /* Tutorial pages */
 router.get('/tutorials', ctrlTutorials.index);
-router.get('/tutorials/view', ctrlTutorials.read);
+router.get('/tutorials/read', ctrlTutorials.read);
 
 module.exports = router;
