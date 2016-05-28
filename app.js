1e772bf79a4ce48c25e67d16d2a30616249b8ce5
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-27 22:28:57 -0500

    add favicon and setup favicon to load

--- app.js
+++ app.js
@@ -11,15 +11,14 @@ var express      = require('express'),
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
 
-
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app configuration
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', routes);
 
