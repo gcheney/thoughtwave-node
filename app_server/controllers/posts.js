f3ab82cc0b8c9c5c7e7757e94b411763d2a5f62c
Glendon Cheney <glendon.cheney@gmail.com>
2016-05-27 19:46:26 -0500

    change thoughts to posts in routes

--- app_server/controllers/posts.js
+++ app_server/controllers/posts.js
@@ -1,14 +1,14 @@
 /* GET thoughts index page */
 module.exports.index = function(req, res) {
-    res.render('thoughts', { title: 'Thought Wave - Thoughts' });
+    res.render('posts/browse', { title: 'Thought Wave - Thoughts' });
 };
 
 /* GET thoughts view page */
 module.exports.read = function(req, res) {
-    res.render('thoughts/read', { title: 'Thought Wave - ' });
+    res.render('posts/read', { title: 'Thought Wave - ' });
 };
 
 /* GET thoughts create page */
 module.exports.create = function(req, res) {
-    res.render('thoughts/create', { title: 'Thought Wave - Create' });
+    res.render('posts/create', { title: 'Thought Wave - Create' });
 };
