2d45ff48d8bd91e3608b6b4d3da7520b0116d97b
Glendon Cheney <glendon.cheney@gmail.com>
2016-06-08 12:24:50 -0500

    commit files before merge

---
+++ public/js/bundle.js
@@ -0,0 +1,44 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// Navigation script to Show Header on Scroll-Up
+$(document).ready(function() {
+    var MQL = 1170;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.navbar-custom').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            }, function()   {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-visible');
+                    } else {
+                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                    }
+                } else {
+                    //if scrolling down...
+                    $('.navbar-custom').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-fixed');
+                    }
+                }
+                this.previousTop = currentTop;
+            }
+        );
+    }
+});
+
+//script to fil in stars on hover 
+$(document).ready(function() {
+    $('.star').hover(function () {
+            $(this).addClass('fa-star');
+            $(this).removeClass('fa-star-o'); 
+        }, function () {
+            $(this).addClass('fa-star-o');
+            $(this).removeClass('fa-star');
+        });
+});
+},{}]},{},[1]);
